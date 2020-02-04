be67af08b1c2565e00b0b64b40e29fc9e521d7eb
caribbeanyute <smithcleon@gmail.com>
2020-02-04 15:05:17 +0000

    Added Profile Page

---
+++ app/utilities/date.py
@@ -0,0 +1,6 @@
+import datetime
+
+
+def format_date_joined(date):
+    return date.strftime("%b, %Y")
+    
--- app/views.py
+++ app/views.py
@@ -7,6 +7,10 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+from .utilities.date import format_date_joined
+import datetime
+
+
 
 
 ###
@@ -24,6 +28,10 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+@app.route('/profile')
+def profile():
+    return render_template('profile.html',date_joined=format_date_joined(datetime.datetime(2020, 5, 17)))
+    
 
 ###
 # The functions below should be applicable to all Flask apps.
